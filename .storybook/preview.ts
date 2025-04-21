7aa5375ff874cdf80d583ade5c94f5dfd0e8c91d
Venisriyallampalli <yvsriy@gmail.com>
2025-04-21 23:47:09 +0530

    Create preview.ts

---
+++ .storybook/preview.ts
@@ -0,0 +1,41 @@
+import type { Preview } from '@storybook/react';
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
+import '../src/index.css';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: '#ffffff',
+        },
+        {
+          name: 'dark',
+          value: '#0f172a',
+        }
+      ],
+    },
+    layout: 'centered',
+  },
+  decorators: [
+    withThemeByDataAttribute({
+      themes: {
+        light: '',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+      attributeName: 'data-theme',
+    }),
+  ],
+};
+
+export default preview;
